39e1ace71c8821f70c53359d5c3efdc2016be146
s1645009 <s1645009@ed.ac.uk>
2020-11-20 16:58:14 +0000

    Added comments to code

--- sequence_analyser.py
+++ sequence_analyser.py
@@ -2,22 +2,29 @@
 import subprocess
 import re
 
+#Defining main body that will run the other functions in correct order/manner
 def main():
+    #Get user input of Taxonomy and Protein for search, choose to continue with search or not
     mySearch, progress = user_search()
     if progress == True:
         print(mySearch)
-        
+#function for determining paramaters for user search
 def user_search():
-    happy = False
-    while happy == False:
+    #loop for giving an option to change search after input in case a mistake was made
+
+    progress = False
+    while progress == False:
+        #user input for Protein family and Taxonomy
         family = input("Enter protein family: ")
         tax = input("Enter Taxanomic group: ")
 
         print(f"Protein family: {family}, Taxanomic group: {tax}")
-        happy = yesNo("Are these correct? Y/N:", "Please re-enter protein family and group.")
+        #choice to continue or not
+        progress = yesNo("Are these correct? Y/N:", "Please re-enter protein family and group.")
 
+    #promting the user to see if they want to exclude partial and or predicted sequences from their analysis
     pred = part = ""
-    ex_predict = yesNo("Do you wish to exclude predictive sequences? Y/N: ", "")
+    ex_predict = yesNo("Do you wish to exclude predicted sequences? Y/N: ", "")
     ex_partial = yesNo("Do you wish to exclude partial sequences? Y/N: ", "")
 
     if ex_predict == True:
@@ -26,27 +33,35 @@ def user_search():
     if ex_partial == True:
         part = "NOT partial"
 
+    #Term that will be searched for on NCBI
     mySearch = "Aves[Organism] AND glucosE-6-phosPhatase[Protein Family] NOT predicted NOT partial"
+    #calling shell command of esearch with specified paramaters and piping results into grep that selects titles of each result
     res = subprocess.check_output(f"esearch -db protein -query \"{mySearch}\" | efetch -format docsum | grep \"<Title>\" ", shell=True)
-
+    
+    #find species names that are in square brackets
     species = re.finditer(r'\[.*?\]', str(res))
-
+    
+    #put species into a list wihout the brackets
     speciesList = []
     for i in species:
         speciesList.append(i.group(0).strip("[]"))
-
+    
+    #total number of results in the list and number of unique species names
     totalResults = len(speciesList)
     speciesNumber = len(set(speciesList))
-    
+
+    #conditions for continuing process
     max_sequences = 1000
     max_species = 500
     progress = True
-
+    
+    #prompt user to continue based on n of seq and species
+    print(f"Number of Sequences: {totalResults}\nNumber of Species: {speciesNumber}" )
     if totalResults > max_sequences:
         progress = yesNo("Warning! Search resulted in more than 1000 sequences. \n do you wish to continue? Y/N: ", "Exiting")
     if speciesNumber > max_species:
         progress = yesNo("Warning! Search resulted in more than 1000 sequences. \n do you wish to continue? Y/N: ", "Exiting")
-    
+
     return mySearch, progress
 
 def fetch_data(mySearch):
